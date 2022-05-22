266b1d7a090082b2cdfc18700a1c837278a76267
Jason A. Wise <jasonandrewwise@gmail.com>
2022-05-21 22:03:11 -0400

    adding funcs
    adding funcs to run logic

--- iWorth/ContentView.swift
+++ iWorth/ContentView.swift
@@ -8,22 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    enum Rate: String, CaseIterable, Identifiable {
-        case yearly, monthly, biweekly, weekly
-        var id: Self { self }
+    @State private var selectedRate = "Yearly"
+    @State private var salary = ""
+    @State private var workingHours = ""
+    @State private var purchaseCost = ""
+    @State private var hourlyRate = 0
+    @State private var purchaseCostPerHour = 0
+    
+    var rates = ["Yearly", "Monthly", "Biweekly", "Weekly"]
+    
+    func selectedRateMapToValues(rate: String) -> Int {
+        switch rate {
+        case "Yearly":
+            return 52
+        case "Monthly":
+            return 12
+        case "Biweekly":
+            return 2
+        case "Weekly":
+            return 1
+        default:
+            return 0
+        }
     }
     
-    @State private var selectedRate: Rate = .yearly
+    func calculatePurchaseCostPerHour(hourlyRate: Int, purchaseCostPerHour: Int) -> Int {
+        return hourlyRate * purchaseCostPerHour
+    }
+    
     var body: some View {
-        VStack{
-            Text("How much do you make: ")
-                .padding()
-            List {
-                Picker("Rate", selection: $selectedRate) {
-                    ForEach(Rate.allCases) { rate in
-                        Text(rate.rawValue.capitalized)
-                    }
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("How much do you make?", text: $salary).padding()
+                    Picker("Rates", selection: $selectedRate) {
+                        ForEach(rates, id:\.self) {
+                            Text($0)
+                        }
+                    }.padding()
                 }
+                
+                TextField("Average hours worked a week", text: $workingHours).padding()
+                
+                TextField("How much does your purchase cost?", text: $purchaseCost).padding()
             }
         }
     }
