5266a47a9bd0c3cc97376bd26a6b4da3adf4db98
Jason A. Wise <jasonandrewwise@gmail.com>
2022-05-21 20:09:34 -0400

    Initial Commit

---
+++ iWorth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iWorth
+//
+//  Created by Jason Wise on 5/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iWorth/iWorthApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWorthApp.swift
+//  iWorth
+//
+//  Created by Jason Wise on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct iWorthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
