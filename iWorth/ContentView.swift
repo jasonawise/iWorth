8979472e227a790b0e681eb0680335e2e0362acc
Jason A. Wise <jasonandrewwise@gmail.com>
2022-05-24 20:19:57 -0400

    check in for the day
    working on refactoring business logic

--- iWorth/ContentView.swift
+++ iWorth/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedRate = "Yearly"
-    @State private var salary = 0
+    @State private var salary = 0.0
     @State private var workingHours = 0
-    @State private var purchaseCost = 0
-    @State private var hourlyRate = 0
+    @State private var purchaseCost = 75.00
+    @State private var hourlyRate = 33.50
     @State private var purchaseCostPerHour = 0
+    @State private var hoursNeededToWork = 0
     
     var rates = ["Yearly", "Monthly", "Biweekly", "Weekly"]
     
@@ -32,35 +33,58 @@ struct ContentView: View {
         }
     }
     
-    func calculatePurchaseCostPerHour(hourlyRate: Int, purchaseCostPerHour: Int) -> Int {
-        return hourlyRate * purchaseCostPerHour
+    func calculateHoursNeededToWork(salary: Double, hourlyRate: Double, purchastCost: Double) -> Double {
+        let rates = selectedRateMapToValues(rate: selectedRate)
+        let weeklyRate = salary / Double(rates)
+        print("Hourly rate is: \(weeklyRate)")
+        return purchastCost / hourlyRate
     }
+//
+//    func calculatePurchaseCostPerHour(hourlyRate: Int, purchaseCostPerHour: Int) -> Int {
+//        return hourlyRate * purchaseCostPerHour
+//    }
+//
+//    func calculateHourlyRate(rate: Int, salary: Int, workingHours: Int) -> Int {
+//        let weeklyRate = salary / rate
+//        let hourlyRate = weeklyRate / workingHours
+//
+//        return hourlyRate
+//    }
+//
+//    func calculateHoursNeededToWork(purchaseCost: Int, hourlyRate: Int ) -> Int {
+//        let hoursNeededToWork = purchaseCost / hourlyRate
+//        return Int(hoursNeededToWork)
+//    }
     
-    func calculateHourlyRate(rate: Int, salary: Int, workingHours: Int) -> Int {
-        let weeklyRate = salary / rate
-        let hourlyRate = weeklyRate / workingHours
-        
-        return hourlyRate
+    func buttonAction() {        
+        print("action")
     }
     
     var body: some View {
         VStack(alignment: .leading) {
-                    Text("How much do you make: ")
-                        
-                    TextField("How much do you make?", value: $salary, format: .currency(code: "USD"))
-                    Picker("Rates", selection: $selectedRate) {
-                        ForEach(rates, id:\.self) {
-                            Text($0)
-                        }
-                    }.pickerStyle(.segmented).padding(.horizontal)
-                Divider().padding(.top)
-                Text("Average hours worked a week: ")
-            TextField("Average hours worked a week", value: $workingHours, format: .number)
-                Divider()
-                Text("How much does your purchase cost?")
-                TextField("How much does your purchase cost?", value: $purchaseCost, format: .currency(code: "USD"))
-            Spacer()
+            Text("How much do you make: ")
+            
+            TextField("How much do you make?", value: $salary, format: .currency(code: "USD"))
+            Picker("Rates", selection: $selectedRate) {
+                ForEach(rates, id:\.self) {
+                    Text($0)
+                }
+            }.pickerStyle(.segmented).padding(.horizontal)
+            Divider().padding(.top)
+            Text("Average hours worked a week: ")
+            TextField("Average hours worked a week", value: $workingHours, format: .number).keyboardType(.decimalPad)
+            Divider()
+            Text("How much does your purchase cost?")
+            TextField("How much does your purchase cost?", value: $purchaseCost, format: .currency(code: "USD"))
+            VStack(alignment: .leading) {
+                Button("Calculate Hours needed to work", action: buttonAction).padding(.top).buttonStyle(.bordered)
+                
+                // Should only show this after the button is clicked
+                TextField("", value: $hoursNeededToWork, format: .number)
+                Spacer()
+            }
         }.padding()
+        
     }
 }
 
